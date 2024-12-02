d5e2eab8d07399267897ab552ddb9deee8f28085
pangsui <upangsuilingeh@gmail.com>
2024-12-02 11:42:50 +0100

    step 4 accessories added

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -61,7 +61,23 @@ function CupcakeList() {
   }, []);
 
   // Step 3: get all accessories
+  const [accessories, setAccessories] = useState<CupcakeArray>([]);
+  useEffect(() => {
+    try {
+      const fetchAccessories = async () => {
+        const response = await fetch("http://localhost:3310/api/accessories");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
 
+        setAccessories(data);
+      };
+      fetchAccessories();
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
   // Step 5: create filter state
 
   return (
@@ -69,11 +85,14 @@ function CupcakeList() {
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
           Filter by{" "}
           <select id="cupcake-select">
-            <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
+            {accessories.length > 0 &&
+              accessories?.map((accessory) => (
+                <option value={accessory.name} key={accessory.id}>
+                  {accessory.name}
+                </option>
+              ))}
           </select>
         </label>
       </form>
